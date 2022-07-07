d4d27e06d327e36bed6abac3bb8265203c121f53
Sovan Patnayak <sovanpatnayak@gmail.com>
2022-07-07 13:09:07 -0400

    Added style components with CSS modules

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,37 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { 
+    container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText 
+} from './layout.module.css'
 
-const Layout = ({ pageTitle, children}) => {
+const Layout = ({ pageTitle, children }) => {
     return (
-        <div>
-            <title>{pageTitle}</title>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                </ul>
-            </nav>
-            <main>
-                <h1>{pageTitle}</h1>
-                {children}
-            </main>
-        </div>
+        // use className prop to apply classes to React Components
+      <div className={container}>
+        <title>{pageTitle}</title>
+        <nav>
+          <ul className={navLinks}>
+            <li className={navLinkItem}>
+              <Link to="/" className={navLinkText}>
+                Home
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/about" className={navLinkText}>
+                About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <h1 className={heading}>{pageTitle}</h1>
+          {children}
+        </main>
+      </div>
     )
 }
 
